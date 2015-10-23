8d7df6cd9597ae38d586d6f67f28838cff033d5b
septor <patrickweaver@gmail.com>
2015-10-23 12:10:47 -0400

    admin_config.php formatting/editing README.mkd updated to add insight on all the commits

--- admin_config.php
+++ admin_config.php
@@ -1,7 +1,4 @@
 <?php
-
-// Generated e107 Plugin Admin Area 
-
 require_once('../../class2.php');
 if (!getperms('P')) 
 {
@@ -9,20 +6,17 @@ if (!getperms('P'))
 	exit;
 }
 
-
-
 class anteup_adminArea extends e_admin_dispatcher
 {
 
 	protected $modes = array(	
 	
-		'other1'	=> array(
+		'main'	=> array(
 			'controller' 	=> 'anteup_ipn_ui',
 			'path' 			=> null,
 			'ui' 			=> 'anteup_ipn_form_ui',
 			'uipath' 		=> null
 		),
-		
 
 		'other1'	=> array(
 			'controller' 	=> 'anteup_currency_ui',
@@ -30,20 +24,15 @@ class anteup_adminArea extends e_admin_dispatcher
 			'ui' 			=> 'anteup_currency_form_ui',
 			'uipath' 		=> null
 		),
-		
-
 	);	
-	
 	
 	protected $adminMenu = array(
 
-		'other1/list'			=> array('caption'=> LAN_MANAGE, 'perm' => 'P'),
-		'other1/create'		=> array('caption'=> LAN_CREATE, 'perm' => 'P'),
-
-		'other1/list'			=> array('caption'=> LAN_MANAGE, 'perm' => 'P'),
-		'other1/create'		=> array('caption'=> LAN_CREATE, 'perm' => 'P'),
-			
-		'main/prefs' 		=> array('caption'=> LAN_PREFS, 'perm' => 'P'),	
+		'main/list'			=> array('caption'=> "Manage Donations", 'perm' => 'P'),
+		'main/create'		=> array('caption'=> "Create Donation", 'perm' => 'P'),
+		'currency/list'		=> array('caption'=> "Manage Currencies", 'perm' => 'P'),
+		'currency/create'	=> array('caption'=> "Create Currency", 'perm' => 'P'),
+		'main/prefs' 		=> array('caption'=> "Preferences", 'perm' => 'P'),	
 
 		// 'main/custom'		=> array('caption'=> 'Custom Page', 'perm' => 'P')
 	);
@@ -52,17 +41,13 @@ class anteup_adminArea extends e_admin_dispatcher
 		'main/edit'	=> 'main/list'				
 	);	
 	
-	protected $menuTitle = 'Ante Up';
+	protected $menuTitle = 'AnteUp';
 }
 
-
-
-
-				
 class anteup_ipn_ui extends e_admin_ui
 {
 			
-		protected $pluginTitle		= 'Ante Up';
+		protected $pluginTitle		= 'AnteUp';
 		protected $pluginName		= 'anteup';
 	//	protected $eventName		= 'anteup-anteup_ipn'; // remove comment to enable event triggers in admin. 		
 		protected $table			= 'anteup_ipn';
@@ -78,32 +63,149 @@ class anteup_ipn_ui extends e_admin_ui
 	
 		protected $listOrder		= 'ipn_id DESC';
 	
-		protected $fields 		= array (  'checkboxes' =>   array ( 'title' => '', 'type' => null, 'data' => null, 'width' => '5%', 'thclass' => 'center', 'forced' => '1', 'class' => 'center', 'toggle' => 'e-multiselect',  ),
-		  'ipn_id' =>   array ( 'title' => 'IPN ID', 'data' => 'int', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'item_name' =>   array ( 'title' => 'Item Name', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'payment_status' =>   array ( 'title' => 'Status', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'mc_gross' =>   array ( 'title' => 'Gross', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'mc_currency' =>   array ( 'title' => 'Currency', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'txn_id' =>   array ( 'title' => 'Transaction ID', 'type' => 'text', 'data' => 'str', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'user_id' =>   array ( 'title' => 'e107 User ID', 'type' => 'number', 'data' => 'str', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'buyer_email' =>   array ( 'title' => 'User Email', 'type' => 'email', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'payment_date' =>   array ( 'title' => 'Payment Date', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'filter' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'comment' =>   array ( 'title' => 'Comment', 'type' => 'textarea', 'data' => 'str', 'width' => '40%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'options' =>   array ( 'title' => LAN_OPTIONS, 'type' => null, 'data' => null, 'width' => '10%', 'thclass' => 'center last', 'class' => 'center last', 'forced' => '1',  ),
+		protected $fields 		= array (  
+			'checkboxes' =>   array ( 
+				'title' => '', 
+				'type' => null, 
+				'data' => null, 
+				'width' => '5%', 
+				'thclass' => 'center', 
+				'forced' => '1', 
+				'class' => 'center', 
+				'toggle' => 'e-multiselect',  
+			),
+		  	'ipn_id' =>   array ( 
+				'title' => 'IPN ID', 
+				'data' => 'int', 
+				'width' => '5%', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'item_name' =>   array ( 
+				'title' => 'Item Name', 
+				'type' => 'text', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'inline' => true, 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'payment_status' =>   array ( 
+				'title' => 'Payment Status', 
+				'type' => 'text', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'mc_gross' =>   array ( 
+				'title' => 'Gross Income', 
+				'type' => 'text', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'mc_currency' =>   array ( 
+				'title' => 'Currency', 
+				'type' => 'text', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'txn_id' =>   array ( 
+				'title' => 'Transaction ID',
+ 			   	'type' => 'text', 
+				'data' => 'str', 
+				'width' => '5%', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left', 
+ 		   	),
+		  	'user_id' =>   array ( 
+				'title' => 'e107 User ID', 
+				'type' => 'number', 
+				'data' => 'str',
+ 			   	'width' => '5%', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'buyer_email' =>   array ( 
+				'title' => 'User Email', 
+				'type' => 'email', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'inline' => true, 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '',
+ 			   	'class' => 'left', 
+				'thclass' => 'left', 
+ 		   	),
+		  	'payment_date' =>   array (
+ 			   	'title' => 'Payment Date', 
+				'type' => 'text', 
+				'data' => 'str',
+ 			   	'width' => 'auto',
+ 			   	'filter' => true,
+ 			   	'help' => '',
+ 			   	'readParms' => '',
+ 			   	'writeParms' => '',
+ 			   	'class' => 'left',
+ 			   	'thclass' => 'left', 
+ 		   	),
+		  	'comment' =>   array ( 
+				'title' => 'Comment', 
+				'type' => 'textarea', 
+				'data' => 'str', 
+				'width' => '40%', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'options' =>   array ( 
+				'title' => LAN_OPTIONS, 
+				'type' => null, 
+				'data' => null, 
+				'width' => '10%', 
+				'thclass' => 'center last',
+ 			   	'class' => 'center last',
+ 			   	'forced' => '1', 
+ 		   	),
 		);		
 		
 		protected $fieldpref = array('item_name', 'payment_date');
-		
-	
+
 		public function init()
 		{
 			// Set drop-down values (if any). 
 	
 		}
-
 		
 		// ------- Customize Create --------
-		
 		public function beforeCreate($new_data)
 		{
 			return $new_data;
@@ -117,8 +219,7 @@ class anteup_ipn_ui extends e_admin_ui
 		public function onCreateError($new_data, $old_data)
 		{
 			// do something		
-		}		
-		
+		}	
 		
 		// ------- Customize Update --------
 		
@@ -136,7 +237,6 @@ class anteup_ipn_ui extends e_admin_ui
 		{
 			// do something		
 		}		
-		
 			
 	/*	
 		// optional - a custom page.  
@@ -147,17 +247,11 @@ class anteup_ipn_ui extends e_admin_ui
 			
 		}
 	*/
-			
 }
-				
-
 
 class anteup_ipn_form_ui extends e_admin_form_ui
 {
-
 }		
-		
-
 				
 class anteup_currency_ui extends e_admin_ui
 {
@@ -178,13 +272,81 @@ class anteup_currency_ui extends e_admin_ui
 	
 		protected $listOrder		= 'id DESC';
 	
-		protected $fields 		= array (  'checkboxes' =>   array ( 'title' => '', 'type' => null, 'data' => null, 'width' => '5%', 'thclass' => 'center', 'forced' => '1', 'class' => 'center', 'toggle' => 'e-multiselect',  ),
-		  'id' =>   array ( 'title' => LAN_ID, 'data' => 'int', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'symbol' =>   array ( 'title' => 'Currency Symbol', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'code' =>   array ( 'title' => 'Code', 'type' => 'number', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'description' =>   array ( 'title' => LAN_DESCRIPTION, 'type' => 'text', 'data' => 'str', 'width' => '40%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'location' =>   array ( 'title' => 'Location', 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'options' =>   array ( 'title' => LAN_OPTIONS, 'type' => null, 'data' => null, 'width' => '10%', 'thclass' => 'center last', 'class' => 'center last', 'forced' => '1',  ),
+		protected $fields 		= array (  
+			'checkboxes' =>   array ( 
+				'title' => '', 
+				'type' => null, 
+				'data' => null, 
+				'width' => '5%', 
+				'thclass' => 'center',
+ 			   	'forced' => '1',
+ 			   	'class' => 'center',
+ 			   	'toggle' => 'e-multiselect',
+  		  	),
+		  	'id' =>   array ( 
+				'title' => LAN_ID, 
+				'data' => 'int', 
+				'width' => '5%', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'symbol' =>   array ( 
+				'title' => 'Currency Symbol',
+ 			   	'type' => 'text',
+ 			   	'data' => 'str',
+ 			   	'width' => 'auto',
+ 			   	'help' => '',
+ 			   	'readParms' => '', 
+				'writeParms' => '',
+ 			   	'class' => 'left',
+ 			   	'thclass' => 'left', 
+ 		   	),
+		  	'code' =>   array ( 
+				'title' => 'Code', 
+				'type' => 'number', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'inline' => true, 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left',  
+			),
+		  	'description' =>   array ( 
+				'title' => LAN_DESCRIPTION, 
+				'type' => 'text', 
+				'data' => 'str', 
+				'width' => '40%', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left', 
+ 		   	),
+		  	'location' =>   array ( 
+				'title' => 'Symbol Location',
+ 			   	'type' => 'text', 
+				'data' => 'str', 
+				'width' => 'auto', 
+				'help' => '', 
+				'readParms' => '', 
+				'writeParms' => '', 
+				'class' => 'left', 
+				'thclass' => 'left', 
+ 		   	),
+		  	'options' =>   array ( 
+				'title' => LAN_OPTIONS, 
+				'type' => null, 
+				'data' => null, 
+				'width' => '10%', 
+				'thclass' => 'center last',
+ 			   	'class' => 'center last', 
+				'forced' => '1',  
+			),
 		);		
 		
 		protected $fieldpref = array();
@@ -195,10 +357,8 @@ class anteup_currency_ui extends e_admin_ui
 			// Set drop-down values (if any). 
 	
 		}
-
 		
 		// ------- Customize Create --------
-		
 		public function beforeCreate($new_data)
 		{
 			return $new_data;
@@ -216,7 +376,6 @@ class anteup_currency_ui extends e_admin_ui
 		
 		
 		// ------- Customize Update --------
-		
 		public function beforeUpdate($new_data, $old_data, $id)
 		{
 			return $new_data;
@@ -231,8 +390,6 @@ class anteup_currency_ui extends e_admin_ui
 		{
 			// do something		
 		}		
-		
-			
 	/*	
 		// optional - a custom page.  
 		public function customPage()
@@ -244,14 +401,10 @@ class anteup_currency_ui extends e_admin_ui
 	*/
 			
 }
-				
-
 
 class anteup_currency_form_ui extends e_admin_form_ui
 {
-
 }		
-		
 		
 new anteup_adminArea();
 
