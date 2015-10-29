74cffd6cda735fe397f3eca2bc16b2fbfacfca68
septor <patrickweaver@gmail.com>
2015-10-29 09:19:55 -0400

    File cleanups

--- _class.php
+++ _class.php
@@ -15,23 +15,8 @@ function format_currency($input, $id, $commify = true)
 	return ($loc == "back" ? $input.$symbol : $symbol.$input);
 }
 
-function config_block($data, $text, $subtext = "")
-{
-	return "<tr>
-	<td style='width:50%;vertical-align:top' class='forumheader3'>
-	<b>".$text."</b>
-	".(!empty($subtext) ? "<br /><span class='smalltext'>".$subtext."</span>" : "")."
-	</td>
-	<td style='width:50%; vertical-align:middle; text-align:right;' class='forumheader3'>
-	".$data."
-	</td>
-	</tr>";
-}
-
 // -- [ Ante Up Constants ]
 
 define("ANTEUP", e_PLUGIN."anteup/");
 define("ANTEUP_ABS", SITEURLBASE.e_PLUGIN_ABS."anteup/");
 define("CALENDAR_IMG", "<img style='vertical-align: middle;' src='".e_PLUGIN."anteup/images/admin/calendar.png' />");
-
-?>
