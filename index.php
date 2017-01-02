0b207d0eadf7aae3576663b632e32f9e2a898625
Tijn Kuyper <github@tijnkuyper.nl>
2017-01-02 13:59:16 +0100

    Update index redirect

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php header('location: donate.php'); ?>
+<?php
+/*
+ * AnteUp - A Donation Tracking Plugin for e107
+ *
+ * Copyright (C) 2012-2017 Patrick Weaver (http://trickmod.com/)
+ * For additional information refer to the README.mkd file.
+ *
+ */
+
+if(!defined('e107_INIT'))
+{
+	require_once('../../class2.php');
+}
+
+e107::redirect('donate.php'); 
