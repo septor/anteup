132fc900601440f2dae7615232434975dfa339db
Norwayman <johnarild.foss@gmail.com>
2017-01-07 12:05:44 -0600

    Spanish Language files
    Spanish Language files

---
+++ languages/Spanish_admin.php
@@ -0,0 +1,69 @@
+<?php
+
+// General
+define("LAN_ANTEUP_MANAGE_DONATIONS", "Administrar donaciones");
+define("LAN_ANTEUP_CREATE_DONATIONS", "Crear donaciones");
+define("LAN_ANTEUP_MANAGE_CURRENCIES", "Administrar Monedas");
+define("LAN_ANTEUP_CREATE_CURRENCIES", "Crear Monedas");
+define("LAN_ANTEUP_CSV_EXPORT", "Exportar a .CSV");
+
+// Donations/IPN
+define("LAN_ANTEUP_IPN_01", "Razon");
+define("LAN_ANTEUP_IPN_02", "Estado de pago");
+define("LAN_ANTEUP_IPN_03", "Ingresos brutos");
+define("LAN_ANTEUP_IPN_04", "Moneda");
+define("LAN_ANTEUP_IPN_05", "ID de transacción");
+define("LAN_ANTEUP_IPN_06", "Fecha de pago");
+define("LAN_ANTEUP_IPN_07", "Comentario");
+
+define("LAN_ANTEUP_IPN_08", "Pendiente");
+define("LAN_ANTEUP_IPN_09", "Terminado");
+define("LAN_ANTEUP_IPN_10", "Negado");
+
+define("LAN_ANTEUP_IPN_11", "¡Gracias por tu trabajo!");
+define("LAN_ANTEUP_IPN_12", "Una generosa donación sin motivo.");
+define("LAN_ANTEUP_IPN_13", "Hosting, servidor, lo que cueste.");
+define("LAN_ANTEUP_IPN_14", "Una donación anónima.");
+define("LAN_ANTEUP_IPN_15", "Se ha creado el archivo .CSV. Ahora se puede importar en Excel, OpenOffice, hojas de cálculo de Google o cualquier otro servicio de hoja de cálculo.");
+define("LAN_ANTEUP_IPN_15", "No se pudo crear el archivo .CSV.");
+
+// Preferences
+define("LAN_ANTEUP_PREFS_01_A", "Título del Menú");
+define("LAN_ANTEUP_PREFS_01_B", "El título de su elemento de menú.");
+define("LAN_ANTEUP_PREFS_02_A", "Moneda predeterminada");
+define("LAN_ANTEUP_PREFS_02_B", "La moneda que desea mostrar en su sitio.");
+define("LAN_ANTEUP_PREFS_03_A", "Objetivo de donación");
+define("LAN_ANTEUP_PREFS_03_B", "La cantidad de donación de tu objetivo.");
+define("LAN_ANTEUP_PREFS_04_A", "Fecha de vencimiento");
+define("LAN_ANTEUP_PREFS_04_B", "La fecha en la que desea obtener su objetivo de donación.");
+define("LAN_ANTEUP_PREFS_05_A", "Fecha de vencimiento anterior");
+define("LAN_ANTEUP_PREFS_05_B", "Su última fecha de vencimiento.");
+define("LAN_ANTEUP_PREFS_06_A", "Texto del menú");
+define("LAN_ANTEUP_PREFS_06_B", "El texto que desea mostrar en su menú de donaciones.");
+define("LAN_ANTEUP_PREFS_07_A", "Texto de la página de donación");
+define("LAN_ANTEUP_PREFS_07_B", "El texto que desea en su página de donación. Que sea romántico.");
+define("LAN_ANTEUP_PREFS_08_A", "Clase de vista de página de donaciones");
+define("LAN_ANTEUP_PREFS_08_B", "Seleccione la clase de usuario que puede ver su página de donaciones. Esto no debe confundirse con donate.php!");
+define("LAN_ANTEUP_PREFS_09_A", "Menú de donación Ver Clase");
+define("LAN_ANTEUP_PREFS_09_B", "Seleccione la clase de usuario que puede ver el menú de donación.");
+define("LAN_ANTEUP_PREFS_10_A", "Imagen del botón de donación");
+define("LAN_ANTEUP_PREFS_10_B", "Imagen para usar en su página de donación y menú de donación.");
+define("LAN_ANTEUP_PREFS_11_A", "PayPal Dirección");
+define("LAN_ANTEUP_PREFS_11_B", "Su dirección de correo electrónico de PayPal. ¡Las donaciones no pueden ser aceptadas sin ella!");
+define("LAN_ANTEUP_PREFS_12_A", "Formato de fecha");
+define("LAN_ANTEUP_PREFS_12_B", "Consulte la función de analizador de toDate() en el manual de desarrolladores de e107.org");
+define("LAN_ANTEUP_PREFS_12_C", "corto");
+define("LAN_ANTEUP_PREFS_12_D", "largo");
+define("LAN_ANTEUP_PREFS_12_E", "relativo");
+define("LAN_ANTEUP_PREFS_13_A", "Modo sandbox");
+define("LAN_ANTEUP_PREFS_13_B", "Habilitar para fines de prueba. El proceso de pago de PayPal se simula en un entorno falso.");
+define("LAN_ANTEUP_PREFS_14_A", "Guardando");
+define("LAN_ANTEUP_PREFS_14_B", "Habilitar para fines de prueba o si lo recomienda un desarrollador. Permite la depuración.");
+
+// Currencies
+define("LAN_ANTEUP_CURR_01", "Símbolo de moneda");
+define("LAN_ANTEUP_CURR_02", "Codigo");
+define("LAN_ANTEUP_CURR_03", "Símbolo Ubicación");
+define("LAN_ANTEUP_CURR_04", "frente");
+define("LAN_ANTEUP_CURR_05", "atrás");
+?>
---
+++ languages/Spanish_front.php
@@ -0,0 +1,43 @@
+<?php
+
+// General
+define("LAN_ANTEUP_MENU_TITLE", "Donaciones");
+define("LAN_ANTEUP_MENU_TEXT", "Gracias por todas sus donaciones, ¡lo apreciamos mucho!");
+define("LAN_ANTEUP_PAGE_TEXT", "Gracias por considerar enviarnos una donación. Apreciamos su contribución y lo utilizaremos para mejorar su experiencia en el sitio");
+
+// donate.php
+define("LAN_ANTEUP_DONATE_TITLE", "Donar");
+
+define("LAN_ANTEUP_DONATE_REASON_01", "¡Gracias por tu trabajo!");
+define("LAN_ANTEUP_DONATE_REASON_02", "Una generosa donación sin motivo.");
+define("LAN_ANTEUP_DONATE_REASON_03", "Hosting, servidor, lo que cueste.");
+define("LAN_ANTEUP_DONATE_REASON_04", "Una donación anónima.");
+
+define("LAN_ANTEUP_DONATE_01", "Razón de la donación");
+define("LAN_ANTEUP_DONATE_02", "Monoda");
+define("LAN_ANTEUP_DONATE_03", "Cantidad");
+define("LAN_ANTEUP_DONATE_04", "Unable to accept donations because there is no PayPal address setup.");
+
+// donations.php
+define("LAN_ANTEUP_DONATIONS_TITLE", "Donations");
+
+define("LAN_ANTEUP_DONATIONS_01", "Donator");
+define("LAN_ANTEUP_DONATIONS_02", "Reason");
+define("LAN_ANTEUP_DONATIONS_03", "Date Donated");
+define("LAN_ANTEUP_DONATIONS_04", "Amount");
+define("LAN_ANTEUP_DONATIONS_05", "Estado");
+define("LAN_ANTEUP_DONATIONS_06", "Total de donaciones de por vida:");
+define("LAN_ANTEUP_DONATIONS_07", "No se encontraron donaciones durante ese período.");
+define("LAN_ANTEUP_DONATIONS_08", "Lo sentimos, amigo, pero no puedes ver esta página en función de tu clase de usuario.");
+
+// return.php
+define("LAN_ANTEUP_RETURN_01", "Pago Cancelado");
+define("LAN_ANTEUP_RETURN_02", "Ha cancelado la transacción. Por favor, considere la posibilidad de hacer una donación en el futuro, realmente lo apreciamos!");
+define("LAN_ANTEUP_RETURN_03", "¡Gracias!");
+define("LAN_ANTEUP_RETURN_04", "¡Gracias por su donación! <br /> <br /> ¡Esté en busca de un correo electrónico de confirmación de PayPal!<br />");
+
+// anteup_menu.php
+define("LAN_ANTEUP_MENU_01", "Fecha de vencimiento");
+define("LAN_ANTEUP_MENU_02", "Estado");
+define("LAN_ANTEUP_MENU_03", "Restante");
+define("LAN_ANTEUP_MENU_04", "Donante (s) más reciente (s)");
