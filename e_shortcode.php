septor/anteup

commit cecd1d04668d1a60fdb0ea63c9a734e7675fb8e7

#43 - Added campaigns filter on donations.php and campaign shortcode fixes

e_shortcode.php
@@ -171,15 +171,29 @@ class anteup_shortcodes extends e_shortcode
 		return "<a href='".e_PLUGIN_ABS."anteup/donate.php'><img src='".e_PLUGIN_ABS."anteup/images/".$pref['anteup_button']."' title='".$pref['anteup_button']."' style='border:none' /></a>";
 	}
 
-	function sc_anteup_campaignselector($parm)
+	function sc_anteup_campaignselector($parm = array())
 	{
 		$frm = e107::getForm();
+		$sql = e107::getDb(); 
 
-		$campaigns = e107::getDb()->retrieve('anteup_campaign', 'name', '', true);
+		$campaigns = array();
 
+		// Add 'All campaigns' option (id = 0) to array
+		$campaigns["0"] =  LAN_ANTEUP_ALL_CAMPAIGNS; 
+
+		// Get existing campaigns from database
+		if($sql->select('anteup_campaign'))
+		{
+			while($row = $sql->fetch())
+			{
+				$campaigns[$row['id']] = $row['name'];
+			}
+		}		
+		
 		$class = (!empty($parm['class']) ? $parm['class'] : "tbox");
+		$selected = (!empty($parm['selected']) ? $parm['selected'] : "");
 
-		$output = $frm->select('campaign', $campaigns, array('class', $class));
+		$output = $frm->select('campaign', $campaigns, $selected, array('class', $class));
 
 		return $output;
 	}
@@ -241,7 +255,8 @@ class anteup_shortcodes extends e_shortcode
 			<tr>
 				<td>".$frm->datepicker('startDate', $this->var[0], 'type=date&format=DD, dd MM, yyyy&size=medium')."</td>
 				<td>".$frm->datepicker('endDate', $this->var[1], 'type=date&format=DD, dd MM, yyyy&size=medium')."</td>
-				<td class='right'>".$frm->button('filterDates', 'Filter', 'submit')."</td>
+				<td>".$this->sc_anteup_campaignselector(array('selected' => $this->var[2]))."</td>
+				<td class='right'>".$frm->button('filterDonations', 'Filter', 'submit')."</td>
 			</tr>
 		</table>";
 		$output .= $frm->close();
@@ -251,12 +266,12 @@ class anteup_shortcodes extends e_shortcode
 
 	function sc_anteup_campaign_name($parm)
 	{
-		return e107::getDb()->retrieve('anteup_campaign', 'name', 'id='.$parm['campaign'].'');
+		return e107::getDb()->retrieve('anteup_campaign', 'name', 'id='.$this->var['campaign'].'');
 	}
 
 	function sc_anteup_campaign_description($parm)
 	{
-		return e107::getDb()->retrieve('anteup_campaign', 'description', 'id='.$parm['campaign'].'');
+		return e107::getDb()->retrieve('anteup_campaign', 'description', 'id='.$this->var['user_id'].'');
 	}
 
 	function sc_anteup_donation_comment($parm='')

donations.php
@@ -22,17 +22,6 @@ $mes = e107::getMessage();
 
 if(check_class($pref['anteup_pageviewclass']))
 {
-	if(isset($_POST['filterDates']))
-	{
-		$startDate 	= (int) $_POST['startDate'];
-		$endDate 	= (int) $_POST['endDate'];
-	}
-	else
-	{
-		$startDate 	= $pref['anteup_lastdue'];
-		$endDate 	= $pref['anteup_due'];
-	}
-
 	$sql = e107::getDb();
 	$tp = e107::getParser();
 	$sc = e107::getScBatch('anteup', true);
@@ -40,11 +29,48 @@ if(check_class($pref['anteup_pageviewclass']))
 	$template = e107::getTemplate('anteup');
 	$template = array_change_key_case($template);
 
-	$sc->setVars(array($startDate, $endDate));
-	$text = $tp->parseTemplate($template['donations']['filter'], false, $sc);
+	// Check if filter has been set 
+	$where_query = "payment_status = 'COMPLETED'";
 
+	if(isset($_POST['filterDonations']))
+	{
+		if(isset($_POST['campaign']))
+		{
+			$campaign = (int) $_POST['campaign'];
 
-	$entries = $sql->retrieve('anteup_ipn', '*', 'payment_date > '.$startDate.' AND payment_date < '.$endDate, true);
+			// if $campaign = 0, it means 'all campaigns' is selected. So do not filter!
+			if($campaign !== 0)
+			{
+				$where_query .= " AND campaign = ".$campaign; 
+			}
+		}
+
+		if(isset($_POST['startDate']))
+		{
+			$startDate = (int) $_POST['startDate']; 
+			$where_query .= " AND payment_date > ".$startDate;
+		}
+
+		
+		if(isset($_POST['endDate']))
+		{
+			$endDate = (int) $_POST['endDate']; 
+			$where_query .= " AND payment_date < ".$endDate;
+		}
+	}
+	// No filters set, just get everything
+	else
+	{
+		$endDate = time();
+	}
+
+	//print_a($where_query);
+	//$entries = $sql->retrieve('anteup_ipn', '*', 'payment_date > '.$startDate.' AND payment_date < '.$endDate, true);
+
+	$entries = $sql->retrieve('anteup_ipn', '*', $where_query, true);
+
+	$sc->setVars(array($startDate, $endDate, $campaign));
+	$text = $tp->parseTemplate($template['donations']['filter'], false, $sc); // TODO also add campaign filter?
 	
 	if($entries)
 	{

languages/English_front.php
@@ -32,3 +32,6 @@ define("LAN_ANTEUP_MENU_02", "Status");
 define("LAN_ANTEUP_MENU_03", "Remaining");
 define("LAN_ANTEUP_MENU_04", "Most recent donator(s)");
 define("LAN_ANTEUP_MENU_05", "Unlimited");
+
+// general 
+define("LAN_ANTEUP_ALL_CAMPAIGNS", "All campaigns"); 

templates/anteup_template.php
@@ -71,7 +71,7 @@ $ANTEUP_TEMPLATE['donations']['start'] = "
 $ANTEUP_TEMPLATE['donations']['entry'] = "
 			<tr>
 				<td>{ANTEUP_DONATION_DONATOR}</td>
-				<td>{ANTEUP_DONATION_CAMPAIGN}</td>
+				<td>{ANTEUP_CAMPAIGN_NAME}</td>
 				<td>{ANTEUP_DONATION_DATE: format=relative}</td>
 				<td>{ANTEUP_DONATION_AMOUNT: format}</td>
 				<td>{ANTEUP_DONATION_STATUS}</td>

commit 9bea395d3ac6530649083014b3732dc0bd3d477d

#43 - Fix {ANTEUP_CAMPAIGN_DESCRIPTION} shortcode
I broke it in https://github.com/septor/anteup/commit/cecd1d04668d1a60fdb0ea63c9a734e7675fb8e7

e_shortcode.php
@@ -271,7 +271,7 @@ class anteup_shortcodes extends e_shortcode
 
 	function sc_anteup_campaign_description($parm)
 	{
-		return e107::getDb()->retrieve('anteup_campaign', 'description', 'id='.$this->var['user_id'].'');
+		return e107::getDb()->retrieve('anteup_campaign', 'description', 'id='.$this->var['campaign'].'');
 	}
 
 	function sc_anteup_donation_comment($parm='')